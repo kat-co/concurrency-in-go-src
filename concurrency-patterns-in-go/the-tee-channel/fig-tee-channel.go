bb10a902ef1bcaf788d2c3ab9475ceb24f05c5fe
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-09-15 19:59:10 +0900

    Update name of variables
    to make it clearer

--- concurrency-patterns-in-go/the-tee-channel/fig-tee-channel.go
+++ concurrency-patterns-in-go/the-tee-channel/fig-tee-channel.go
@@ -73,14 +73,14 @@ func main() {
 			defer close(out1)
 			defer close(out2)
 			for val := range orDone(done, in) {
-				var out1, out2 = out1, out2
+				var out1copy, out2copy = out1, out2
 				for i := 0; i < 2; i++ {
 					select {
 					case <-done:
-					case out1 <- val:
-						out1 = nil
-					case out2 <- val:
-						out2 = nil
+					case out1copy <- val:
+						out1copy = nil
+					case out2copy <- val:
+						out2copy = nil
 					}
 				}
 			}
